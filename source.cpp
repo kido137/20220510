bba810f258e46789043088b854cc4e8694e4afc7
CzDelta <kido137@naver.com>
2022-05-10 11:00:06 +0900

    정렬 추가

--- source.cpp
+++ source.cpp
@@ -2,8 +2,46 @@
 
 using namespace std;
 
+int ArrayAdd(int* list, int b)
+{
+	int result = 0;
+
+	for (int i = 0; i < b; i++)
+	{
+		result += list[i];
+	}
+
+	return result;
+}
+
+//void ArraySort(int list[], int n)
+//{
+//	for (int i = 0; i < i++)
+//}
+
 int main()
 {
-	return 0;
+	int n = 0;
+	cin >> n;
 
+	int* arr = new int[n];
+
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+
+	cout << ArrayAdd(arr, n);
+
+	//ArraySort(arr, n);
+
+	/*for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << endl;
+	}*/
+
+	delete[] arr;
+
+	return 0;
 }
